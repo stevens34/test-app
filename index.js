b00e07443dc0f37a287a91aa72f1a386c5cc1996
stevens34 <ssisjayawan@gournayconsulting.com>
2022-04-20 13:26:16 -0400

    pack

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = 5000;
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: '/index.html'});      //server responds by sending the index.html file to the client's browser
-                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Now listen on port ${port}`);
 })
